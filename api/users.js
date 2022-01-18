f081282ca8afde379fdaa27ab6d2c8eb5131e153
shirus172 <shirus172@gmail.com>
2022-01-18 11:56:06 -0700

    Added more user endpoints

--- api/users.js
+++ api/users.js
@@ -13,24 +13,42 @@ router.use(bodyParser.json({ extended: true }));
 
 //defind the users route
 
+//List Users
   router.get('/', function (req, res) {
     console.log('list users')
     api_helper.make_API_call('/users', 'GET')
     .then(result => res.status(200).send(result))
     .catch(err => res.status(500).send(err));
   })
-
+//Get  a User
+  router.get('/:id', function (req, res) {
+    console.log('list users')
+    api_helper.make_API_call(`/users/${req.query.id}`, 'GET')
+    .then(result => res.status(200).send(result))
+    .catch(err => res.status(500).send(err));
+  })
+//Create a User
   router.post('/', function (req, res) {
     console.log(`Body: ${req.body}`)
     const data = req.body
-    //console.log(data)
     api_helper.make_API_call('/users', 'POST', data)
     .then(result => res.status(200).send(result))
     .catch(err => res.status(500).send(err));
   })
-
-  router.patch('/', function (req, res) {
-    res.send('Update a User');
+//Update a User
+  router.patch('/:id', function (req, res) {
+    const data = req.body
+    api_helper.make_API_call(`/users/${req.query.id}`, 'PATCH', data)
+    .then(result => res.status(200).send(result))
+    .catch(err => res.status(500).send(err));
   })
   
+//Delete a User
+router.delete('/:id', function (req, res) {
+  const data = req.body
+  api_helper.make_API_call(`/users/${req.query.id}`, 'DELETE', data)
+  .then(result => res.status(200).send(result))
+  .catch(err => res.status(500).send(err));
+})
+
 module.exports = router;
